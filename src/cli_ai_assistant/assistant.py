d8ca573071a87f0d9c97cb395ce79d81d08c612c
Chaman Rawat <chamanrawat.dev@gmail.com>
2026-08-27 01:53:28 +0530

    feat: initialize OpenAI client and system prompt

--- src/cli_ai_assistant/assistant.py
+++ src/cli_ai_assistant/assistant.py
@@ -1,3 +1,22 @@
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+client = OpenAI(
+    base_url=os.getenv("OPENROUTER_BASE_URL"), api_key=os.getenv("OPENROUTER_API_KEY")
+)
+
+system_prompt = {"role": "system", "content": "You are a helpful Assistant."}
+messages = []
+
+
+def start_repl():
+    pass
+
+
 def main():
     print("Hello from assistant!")
 
